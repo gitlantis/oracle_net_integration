8afa7c43806b910c423cef05e4200f69ca4b97d2
gitlantis <a.oripov@outlook.com>
2020-11-04 11:11:52 +0500

    Add project files.

---
+++ oracle_net_integration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oracle_net_integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
